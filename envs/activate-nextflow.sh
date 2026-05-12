406689eeef9883be68b0b2cbd5da388c710f176f
ocean-debug <ocean-debug@users.noreply.github.com>
2026-05-12 22:58:26 +0800

    Scope Nextflow runtime to project tools

--- envs/activate-nextflow.sh
+++ envs/activate-nextflow.sh
@@ -7,4 +7,5 @@ CODEX_OMICS_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 export CODEX_OMICS_HOME
 
 export JAVA_HOME="$CODEX_OMICS_HOME/tools/java/jdk-17"
+export NXF_HOME="$CODEX_OMICS_HOME/tools/nextflow/.nextflow"
 export PATH="$JAVA_HOME/bin:$CODEX_OMICS_HOME/tools/nextflow:$PATH"
